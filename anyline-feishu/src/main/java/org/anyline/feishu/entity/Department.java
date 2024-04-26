046a6174f51ea9815d9f368635b018ee997613d6
anyline <server@anyline.org>
2024-04-26 14:12:03 +0800

    fs entity

---
+++ anyline-feishu/src/main/java/org/anyline/feishu/entity/Department.java
@@ -0,0 +1,49 @@
+package org.anyline.feishu.entity;
+
+public class Department {
+    private String code;
+    private String openid;
+    private String name;
+    private String parentCode;
+    private String leaderCode;
+
+    public String getOpenid() {
+        return openid;
+    }
+
+    public void setOpenid(String openid) {
+        this.openid = openid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getParentCode() {
+        return parentCode;
+    }
+
+    public void setParentCode(String parentCode) {
+        this.parentCode = parentCode;
+    }
+
+    public String getLeaderCode() {
+        return leaderCode;
+    }
+
+    public void setLeaderCode(String leaderCode) {
+        this.leaderCode = leaderCode;
+    }
+}
